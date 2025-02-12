4f210e8c3e1f8056692bd140257ab641d5c6f30c
kylelee-dev <leekyle.dev@gmail.com>
2025-02-12 15:43:18 -0500

    feat: implement user update endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,3 +20,18 @@ exports.getUser = async (req, res) => {
     res.status(404).json({ error: error.message });
   }
 };
+
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    return res.status(400).json({ error: error.message});
+  }
+};
